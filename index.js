241148f78d94f3aab6e548a4d7f9dfd2e1a9f15e
James Tapsell <email@jrtapsell.co.uk>
2017-03-31 15:21:53 +0100

    Extracted javascript to new file

---
+++ index.js
@@ -0,0 +1,84 @@
+function cardify(outer, item) {
+  var card = $("<div/>", {
+    "class": "mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col"
+  });
+  var image_wrapper = $("<div/>", {
+    "class": "image-wrapper"
+  });
+  var title_div = $("<div/>", {
+    "class": "mdl-card__title"
+  });
+  var title = $("<h2/>", {
+    "class": "mdl-card__title-text card-title"
+  });
+  var description = $("<div/>", {
+    "class": "mdl-card__supporting-text"
+  });
+  var link_div = $("<div/>", {
+    "class": "mdl-card__actions mdl-card--border"
+  });
+  var link = $("<a/>", {
+    "class": "mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"
+  });
+  title_div.css("background", "url('" + item["icon"] + "') no-repeat center ");
+  title_div.css("background-size", "contain");
+  image_wrapper.css("background", item["backing"]);
+  description.text(item["description"]);
+  card.appendTo(outer);
+  title_div.appendTo(image_wrapper);
+  image_wrapper.appendTo(card);
+  description.appendTo(card);
+  title.appendTo(title_div);
+  title.text(item["name"]);
+  link_div.appendTo(card);
+  link.appendTo(link_div);
+  link.text("Go To");
+  link.attr("href", item["url"])
+}
+
+function onSuccess(data) {
+  var target = $("#target");
+  var linksbar = $("#linksbar");
+  $.each(data, function(key, value) {
+    var link = $("<a/>", {
+      "class": "mdl-navigation__link"
+    });
+    link.text(key);
+    link.attr("href", "#" + key);
+    link.appendTo(linksbar);
+    var bar = $("<h2/>", {
+      "id": key
+    });
+    bar.text(key);
+    bar.appendTo(target);
+    var projects = $("<div/>", {
+      "class": "card-container mdl-grid"
+    });
+    projects.appendTo(target);
+    console.log(key, value);
+    $.each(value, function(_, item) {
+      cardify(projects, item);
+    });
+  });
+}
+function onError(data, text, error) {
+  console.log(data, text, error);
+}
+
+$.ajax({
+  url: "data.json",
+  data: "",
+  success: onSuccess,
+  error: onError,
+  dataType: "json"
+});
+function onAbout(response) {
+  $("#about").html(response);
+}
+$.ajax({
+  url: "about.html",
+  data: "",
+  success: onAbout,
+  error: onError,
+  dataType: "html"
+});
