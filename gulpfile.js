dc13dab84952127f81db50a57a5d5b45587036a2
James Tapsell <email@jrtapsell.co.uk>
2017-05-03 15:37:00 +0100

    Added minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var handlebars = require('gulp-compile-handlebars');
 var rename = require('gulp-rename');
+var htmlmin = require('gulp-htmlmin');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('render', function () {
   var templateData = require("./src/data/data.json");
@@ -8,12 +10,14 @@ gulp.task('render', function () {
   return gulp.src('src/hbs/index.hbs')
     .pipe(handlebars(templateData))
     .pipe(rename('index.html'))
+    .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('dist'));
 });
 
 
 gulp.task('css', function(){
   return gulp.src('src/css/index.css')
+    .pipe(cleanCSS())
     .pipe(gulp.dest("dist"))
 });
 
