61fc6ba92b9c56d87dc132d517aa46d477e6fc9c
James Tapsell <email@jrtapsell.co.uk>
2017-06-28 00:04:24 +0100

    Refactored service worker installer

--- src/js/service_worker.js
+++ src/js/service_worker.js
@@ -1,27 +1,37 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js').then(function(reg) {
-      reg.onupdatefound = function() {
-        var installingWorker = reg.installing;
+function onSwUpdate(reg) {
+  return function () {
+    var installingWorker = reg.installing;
 
-        installingWorker.onstatechange = function() {
-          switch (installingWorker.state) {
-            case 'installed':
-              if (navigator.serviceWorker.controller) {
-                console.log('New or updated content is available.');
-              } else {
-                console.log('Content is now available offline!');
-              }
-              break;
-
-            case 'redundant':
-              console.error('The installing service worker became redundant.');
-              break;
+    installingWorker.onstatechange = function() {
+      switch (installingWorker.state) {
+        case 'installed':
+          if (navigator.serviceWorker.controller) {
+            console.log('New or updated content is available.');
+          } else {
+            console.log('Content is now available offline!');
           }
-        };
-      };
-    }).catch(function(e) {
-      console.error('Error during service worker registration:', e);
-    });
-  });
+          break;
+
+        case 'redundant':
+          console.error('The installing service worker became redundant.');
+          break;
+      }
+    };
+  }
+}
+
+function onSwSuccess(reg) {
+  reg.onupdatefound = onSwUpdate(reg);
+}
+
+function onSwFail(e) {
+  console.error('Error during service worker registration:', e);
+}
+
+function onSwLoad() {
+  navigator.serviceWorker.register('/service-worker.js').then(onSwSuccess).catch(onSwFail);
+}
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', onSwLoad);
 }
