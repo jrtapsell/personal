5dc4fd768781f8353bf390f4c3ef30fda7167e42
James Tapsell <email@jrtapsell.co.uk>
2017-12-05 12:40:14 +0000

    Moved from single to double quotes

--- src/js/service_worker.js
+++ src/js/service_worker.js
@@ -1,15 +1,15 @@
 function stateChange(newState) {
   switch (newState) {
-    case 'installed':
+    case "installed":
       if (navigator.serviceWorker.controller) {
-        console.log('New or updated content is available.');
+        console.log("New or updated content is available.");
       } else {
-        console.log('Content is now available offline!');
+        console.log("Content is now available offline!");
       }
       break;
 
-    case 'redundant':
-      console.error('The installing service worker became redundant.');
+    case "redundant":
+      console.error("The installing service worker became redundant.");
       break;
   }
 }
@@ -31,13 +31,13 @@ function onSwSuccess(reg) {
 }
 
 function onSwFail(e) {
-  console.error('Error during service worker registration:', e);
+  console.error("Error during service worker registration:", e);
 }
 
 function onSwLoad() {
-  navigator.serviceWorker.register('/service-worker.js').then(onSwSuccess).catch(onSwFail);
+  navigator.serviceWorker.register("/service-worker.js").then(onSwSuccess).catch(onSwFail);
 }
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', onSwLoad);
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", onSwLoad);
 }
