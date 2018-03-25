db7b6bf7bccb53e5edf73b397db654cacd1a479e
James Tapsell <email@jrtapsell.co.uk>
2018-03-25 17:17:41 +0100

    Added comments and JS minification

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,8 @@ const EXTERNAL = {
   "siteMap": require("gulp-sitemap"),
   "serviceWorker": require("sw-precache"),
   "imageMin": require("gulp-imagemin"),
-  "fileSystem": require("fs")
+  "fileSystem": require("fs"),
+  "uglify": require('gulp-uglify')
 };
 
 const ALL_TASKS = [];
@@ -18,19 +19,27 @@ function defineTask(name, action) {
   ALL_TASKS.push(name);
 }
 
-
+/**
+ * Optimises the CSS
+ */
 defineTask("css", function () {
   return EXTERNAL.gulp.src("src/css/index.css")
     .pipe(EXTERNAL.cleanCSS())
     .pipe(EXTERNAL.gulp.dest("dist"));
 });
 
+/**
+ * Minifies the JS
+ */
 defineTask("js", function () {
   return EXTERNAL.gulp.src("src/js/*.js")
+    .pipe(EXTERNAL.uglify())
     .pipe(EXTERNAL.gulp.dest("dist/js"));
 });
 
-
+/**
+ * Compresses the images
+ */
 defineTask("img", function () {
   return EXTERNAL.gulp.src("src/img/*")
     .pipe(EXTERNAL.imageMin())
@@ -38,11 +47,17 @@ defineTask("img", function () {
 });
 
 
+/**
+ * Brings in the fonts
+ */
 defineTask("font", function () {
   return EXTERNAL.gulp.src("src/res/min-font/**")
     .pipe(EXTERNAL.gulp.dest("dist/res/min-font"));
 });
 
+/**
+ * Creates all of the site pages
+ */
 defineTask("pages", function () {
   return EXTERNAL.gulp.src("src/hbs/pages/*.hbs")
     .pipe(EXTERNAL.handlebars(null, {
@@ -64,17 +79,26 @@ defineTask("pages", function () {
     .pipe(EXTERNAL.gulp.dest("dist"));
 });
 
+/**
+ * Brings in the CSS files
+ */
 defineTask("mdl-css", function () {
   return EXTERNAL.gulp.src("src/res/mdl/custom.css")
     .pipe(EXTERNAL.cleanCSS())
     .pipe(EXTERNAL.gulp.dest("dist/res/mdl"));
 });
 
+/**
+ * Creates the manifest
+ */
 defineTask("manifest", function () {
   return EXTERNAL.gulp.src("src/manifest/manifest.json")
     .pipe(EXTERNAL.gulp.dest("dist"));
 });
 
+/**
+ * Makes the service worker
+ */
 defineTask("make-sw", function (callback) {
   EXTERNAL.serviceWorker.write("dist/service-worker.js", {
     staticFileGlobs: ["dist/**/*"],
@@ -82,18 +106,26 @@ defineTask("make-sw", function (callback) {
   }, callback);
 });
 
+/**
+ * Builds the sitemap
+ */
 defineTask("sitemap", function () {
   return EXTERNAL.gulp.src("dist/*.html", {read: false})
-    .pipe(EXTERNAL.gulpIgnore.exclude("google*.html"))
     .pipe(EXTERNAL.siteMap({
       siteUrl: "http://www.jrtapsell.co.uk"
     }))
     .pipe(EXTERNAL.gulp.dest("./dist"));
 });
 
+/**
+ * Adds things that shouldn't be cached and shouldn't be in the sitemap
+ */
 defineTask("extra", function () {
   return EXTERNAL.gulp.src(["extra/*", "extra/.**/*"])
     .pipe(EXTERNAL.gulp.dest("dist"));
 });
 
+/**
+ * Creates the default task
+ */
 EXTERNAL.gulp.task("default", EXTERNAL.gulp.series(ALL_TASKS));
