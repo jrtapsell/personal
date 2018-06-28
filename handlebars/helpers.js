168cf8172ffa8a6aaa142a7dae1860cca21433e3
James Tapsell <email@jrtapsell.co.uk>
2018-06-28 20:06:16 +0100

    Restyled button

--- handlebars/helpers.js
+++ handlebars/helpers.js
@@ -37,14 +37,32 @@ function withJsonFile(filename, options) {
   return safeString(options.fn(data));
 }
 
+function getResult(url, text) {
+    return `<a href="${url}">
+    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+        ${text}
+    </button>
+    </a>`;
+}
+
+function makeButton(url, text) {
+    return `<a href="${url}" class="button-link">
+    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent button-body">
+        ${text}
+    </button>
+    </a>`
+}
+
+function button(data) {
+    const {text, url} = data.hash;
+    return safeString(makeButton(url, text))
+}
 function buttonBar(body) {
   let result = "<div class=\"mdl-cell mdl-cell--12-col button-bar\">";
   for (const buttonDetails of JSON.parse(body)) {
-    result += `<a href="${buttonDetails[1]}">
-    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-        ${buttonDetails[0]}
-    </button>
-    </a>`;
+      const url = buttonDetails[1];
+      const text = buttonDetails[0];
+      result += makeButton(url, text);
   }
   result += "</div>";
   return safeString(result);
@@ -57,7 +75,8 @@ const helpers = {
   "youtube": wrap("<iframe src=\"https://www.youtube.com/embed/", "\" frameborder=\"0\" allowfullscreen></iframe>"),
   "vimeo": wrap("<iframe src=\"https://player.vimeo.com/video/", "\" frameborder=\"0\" allowfullscreen></iframe>"),
   "pdf": wrap("<iframe src=\"https://docs.google.com/gview?url=", "&embedded=true\" frameborder=\"0\"></iframe>"),
-  "button_bar": buttonBar
+  "button_bar": buttonBar,
+  "button": button
 };
 
 module.exports.helpers = helpers;
